abdd4b3442ee328205b53b6842f0ed00228e0022
cabolton <adarga12@gmail.com>
2019-09-03 20:41:17 -0700

    Varying sample data timestamps.

--- src/main/java/com/adarga/data/InMemoryDataStore.java
+++ src/main/java/com/adarga/data/InMemoryDataStore.java
@@ -3,6 +3,7 @@ package com.adarga.data;
 import com.adarga.domain.Goal;
 import com.adarga.domain.Metric;
 import com.adarga.domain.Tracker;
+import org.joda.time.DateTime;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -20,7 +21,7 @@ public class InMemoryDataStore implements DataStore {
     private ArrayList<Tracker> trackers;
 
     private InMemoryDataStore(){
-        metrics = new ArrayList<Metric>();
+        metrics = new ArrayList<>();
         initializeMetrics();
     }
 
@@ -32,13 +33,14 @@ public class InMemoryDataStore implements DataStore {
     }
 
     private void initializeMetrics() {
-        metrics.add(new Metric(0, 5.2f));
+        metrics.add(new Metric(0, 5.2f, new DateTime(2019,6,13,11,33)));
         Goal goal = new Goal(0, 10.0f, "Cardio", "Minutes spent on cardiovascular exercise");
-        metrics.add(new Metric(0, 7.6f));
-        metrics.add(new Metric(0, 8.1f));
-        metrics.add(new Metric(0, 2.98f));
+        metrics.add(new Metric(0, 7.6f,  new DateTime(2019,7,4,10,45)));
+        metrics.add(new Metric(0, 8.1f,  new DateTime(2019,7,19,13,12)));
+        metrics.add(new Metric(0, 2.98f,  new DateTime(2019,8,5,15,26)));
+        metrics.add(new Metric(0, 3.1f,  new DateTime(2019,9,2,9,8)));
         tracker = new Tracker(goal, metrics);
-        trackers = new ArrayList<Tracker>();
+        trackers = new ArrayList<>();
         trackers.add(tracker);
     }
 
